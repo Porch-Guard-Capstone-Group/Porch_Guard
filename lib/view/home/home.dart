53d4932e79f49df8b1456cb1c3d846d420759dfb
Abunie <gaiya@ualberta.ca>
2021-02-16 01:10:51 -0700

    Delivery Page

--- lib/view/home/home.dart
+++ lib/view/home/home.dart
@@ -8,8 +8,61 @@ class _HomeState extends State<Home>{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-        body: Text(" Home"),
-
+        body: Container(
+          padding: EdgeInsets.symmetric(vertical: 20.0 , horizontal: 50.0),
+            child: ListView(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(top:50),
+                  child: ButtonTheme(
+                    height: 50,
+                    child:RaisedButton(
+                      color: Colors.blue,
+                      child: Text('TURN OFF ALARM', style: TextStyle(color: Colors.white, fontSize: 20,)),
+                      onPressed: () async{
+                      },
+                    ) ,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top:50),
+                  child: ButtonTheme(
+                    height: 50,
+                    child:RaisedButton(
+                      color: Colors.blue,
+                      child: Text('TURN ON ALARM', style: TextStyle(color: Colors.white, fontSize: 20,)),
+                      onPressed: () async{
+                      },
+                    ) ,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top:50),
+                  child: ButtonTheme(
+                    height: 50,
+                    child:RaisedButton(
+                      color: Colors.blue,
+                      child: Text('TURN ON ACCELEROMETER', style: TextStyle(color: Colors.white, fontSize: 20,)),
+                      onPressed: () async{
+                      },
+                    ) ,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top:50),
+                  child: ButtonTheme(
+                    height: 50,
+                    child:RaisedButton(
+                      color: Colors.blue,
+                      child: Text('TURN ON MOTION SENSOR', style: TextStyle(color: Colors.white, fontSize: 20,)),
+                      onPressed: () async{
+                      },
+                    ) ,
+                  ),
+                )
+              ],
+            )
+        ),
     );
   }
 }
