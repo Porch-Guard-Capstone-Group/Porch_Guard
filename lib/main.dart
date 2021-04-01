c58005c9efc2b3ef46874501a7511dce31d2dab5
Abunie <gaiya@ualberta.ca>
2021-03-31 21:55:22 -0600

    FCM Notifications

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,55 @@ import 'package:porchguard/view/Sign_In/signin.dart';
 import 'package:porchguard/view/base/base.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
 }
-class MyApp extends StatelessWidget{
+
+class MyApp extends StatefulWidget{
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+class _MyAppState extends State<MyApp>{
+  String textValue = 'Hello World';
+  FirebaseMessaging firebaseMessaging = new FirebaseMessaging();
+  @override
+  void initState(){
+    firebaseMessaging.configure(
+      onLaunch: (Map<String, dynamic> msg){
+        print(" onLaunch called");
+      },
+      onResume: (Map<String, dynamic> msg){
+        print("onResume called");
+      },
+      onMessage: (Map<String, dynamic> msg){
+        print(" onMessage called $msg");
+      },
+    );
+    firebaseMessaging.requestNotificationPermissions(
+      const IosNotificationSettings(
+        sound: true,
+        alert: true,
+        badge: true,
+      )
+    );
+    firebaseMessaging.onIosSettingsRegistered.listen((IosNotificationSettings settings) {
+      print("IOS Settings Registered");
+    });
+    firebaseMessaging.getToken().then((token){
+      update(token);
+    });
+  }
+
+  update(String token){
+    print("Token" + token);
+    textValue = token;
+    setState((){});
+  }
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -28,7 +70,7 @@ class MyApp extends StatelessWidget{
             primarySwatch: Colors.blue,
             visualDensity: VisualDensity.adaptivePlatformDensity
         ) ,
-        home:AuthenticationWrapper(),
+         home:AuthenticationWrapper(),
       ),
     );
   }
